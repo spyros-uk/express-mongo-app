557a3d92381517f5ed3a6e49649554f225327bb5
Spyros <dev@spyros.uk>
2020-04-27 22:41:18 +0100

    Adds morgan middleware

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express"
+import morgan from "morgan"
 import { registerToursRoutes } from "./api/tours"
 
 const PORT = 3000
 const app = express()
 
+app.use(express.json())
+app.use(morgan("dev"))
+
 registerToursRoutes(app)
 
-app.use(express.json())
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`)
 })
